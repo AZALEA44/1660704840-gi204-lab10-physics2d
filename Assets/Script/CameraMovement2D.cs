1fa62a68e783d2b3289ff20f227591bd4f62c624
AZALEA44 <midnightbestever@gmail.com>
2025-04-24 23:24:04 +0700

    finish
    finish

---
+++ Assets/Script/CameraMovement2D.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraMovement2D : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
+
+
+
+
